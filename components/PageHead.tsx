8ca38358d035285c8a3e145fc73c2681e3bb933c
mecovibre <132189212+mecovibre@users.noreply.github.com>
2023-07-31 00:01:56 +0900

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -63,6 +63,10 @@ export const PageHead: React.FC<
         title={site?.name}
       />
 
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR&display=swap" rel="stylesheet">
+  
       <meta property='og:title' content={title} />
       <meta name='twitter:title' content={title} />
       <title>{title}</title>
